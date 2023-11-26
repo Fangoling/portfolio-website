494c53b717789a3a54bc38bdddd49397ecbf5122
fxliu <ge83toc@mytum.de>
2023-11-26 09:27:24 +0100

    fixed warnings for eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
-        <title>Fangxing Liu's website</title>
-        <link rel="stylesheet" href="styles/index.css"/>
+        <title>Fangxing Liu&apos;s website</title>
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
       </head>
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main>
       <div className="home-content">
-        <h1>Hi, I'm Fangxing Liu</h1>
+        <h1>Hi, I&apos;m Fangxing Liu</h1>
         <h3>CS Student at TUM</h3>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus exercitationem nam quia hic, minus earum possimus nihil iusto sunt accusantium?</p>
         <div className="btn-box">
-            <a href="mailto:fxliu666@gmail.com">Let's Talk</a>
+            <a href="mailto:fxliu666@gmail.com">Let&apos;s Talk</a>
             <a href="https://www.linkedin.com/in/fangxingliu">Hire Me</a>
         </div>
       </div> 
