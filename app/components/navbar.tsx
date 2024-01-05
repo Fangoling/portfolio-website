af8747dd067098f3e9595729a40a48c4497a8b9f
Fangxing Liu <ge83toc@mytum.de>
2024-01-05 16:38:54 +0100

    added meta data to my website and put the navbar in a react component

---
+++ app/components/navbar.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { usePathname } from "next/navigation"
+import React from "react"
+
+
+export default function Navbar() {
+    const pathname = usePathname() 
+
+    return <nav className="navbar">
+        <a href="/"className={pathname === '/' ? 'active' : ''}>Home</a>
+        <a href="/about"className={pathname === '/about' ? 'active' : ''}>About</a>
+        <a href="/portfolio" className={pathname === '/portfolio' ? 'active' : ''}>Portfolio</a>
+        <a href="/services" className={pathname === '/services' ? 'active' : ''}>Services</a>
+        <a href="/contact" className={pathname === '/contact' ? 'active' : ''}>Contact</a>
+    </nav>
+}
