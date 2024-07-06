250b3609eb9ad1bdddec726e3af2f43804f4208a
fxliu <ge83toc@mytum.de>
2024-07-06 23:27:44 +0200

    removed services from navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,11 +6,13 @@ import React from "react"
 export default function Navbar() {
     const pathname = usePathname() 
 
-    return <nav className="navbar">
+    return(
+    <nav className="navbar">
         <a href="/"className={pathname === '/' ? 'active' : ''}>Home</a>
         <a href="/about"className={pathname === '/about' ? 'active' : ''}>About</a>
         <a href="/portfolio" className={pathname === '/portfolio' ? 'active' : ''}>Portfolio</a>
-        <a href="/services" className={pathname === '/services' ? 'active' : ''}>Services</a>
         <a href="/contact" className={pathname === '/contact' ? 'active' : ''}>Contact</a>
     </nav>
+    )
+
 }
