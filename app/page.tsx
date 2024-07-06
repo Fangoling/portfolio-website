1dfc10865ec8ae3dd9b1ee99d1393b2a274980b4
fxliu <ge83toc@mytum.de>
2024-07-06 23:26:48 +0200

    removed hire me and contact me buttons, redundant

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import './global.css'
 import Image from 'next/image'
+import {Button} from '@nextui-org/button'
 
 export default function Home() {
   return (
@@ -8,17 +9,7 @@ export default function Home() {
         <h1>Hi, I&apos;m Fangxing Liu</h1>
         <h3>Computer Science Student</h3>
         <p>As an aspiring programmer with proficiency in Android Jetpack, Java, and C, I am looking forward to gain experience and enhance my skills. Currently, I am pursuing my studies at the Technical University of Munich.</p>
-        <div className="btn-box">
-            <a href="mailto:fxliu666@gmail.com">Let&apos;s Talk</a>
-            <a href="https://www.linkedin.com/in/fangxingliu">Hire Me</a>
-        </div>
       </div> 
-    <Image 
-        src="/cat-images/cat-coding.gif"
-        alt="cat coding gif"
-        width={450}
-        height={450}
-      />
     </main>
   )
 }
