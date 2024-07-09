e42026f91b67cbec6e8c91bc8021e0ae088a4615
Fangxing Liu <ge83toc@mytum.de>
2024-07-09 23:30:31 +0200

    added new project to portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Spacer } from "@nextui-org/react";
 
 export default function Portfolio() {
   return (
+    <div>
     <Card className="max-w-[100px] bg-primary text-font">
       <CardHeader className="flex gap-3">
         <div className="flex flex-col">
@@ -21,12 +22,38 @@ export default function Portfolio() {
         <Link
           isExternal
           showAnchorIcon
-          href="https://www.fangxingliu.com"
+          href="https://github.com/Fangoling/portfolio-website"
           className='text-font'
         >
           Visit source code on GitHub.
         </Link>
       </CardFooter>
     </Card>
+    <Spacer y={4}/>
+    <Card className="max-w-[100px] bg-primary text-font">
+      <CardHeader className="flex gap-3">
+        <div className="flex flex-col">
+          <p className="text-md">BrainRot GPT</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+        <p>
+          Eine Chainlit App, welche ein Transkipt von einem Meeting in ein Video umwandelt und mit Untertiteln vorliest. 
+        </p>
+      </CardBody>
+      <Divider/>
+      <CardFooter>
+        <Link
+          isExternal
+          showAnchorIcon
+          href="https://github.com/Fangoling/BrainRotGPT"
+          className='text-font'
+        >
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+    </div>
   )
 }
