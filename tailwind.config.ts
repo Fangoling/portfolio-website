b90cd45eeb3769e3283f4a178e15d9eef9846383
fxliu <ge83toc@mytum.de>
2024-07-12 00:38:29 +0200

    added screen sizes to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,13 @@ const config: Config = {
         
       },
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   darkMode: "class",
   plugins: [nextui()],
