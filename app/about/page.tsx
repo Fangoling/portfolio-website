997f0149ad641f68d375b53ba3384616123c801d
fxliu <ge83toc@mytum.de>
2024-07-11 23:55:54 +0200

    added some placeholder for the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,20 +3,24 @@ import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nex
 
 export default function About() {
   return (
-    <Card className="max-w-[100px] bg-primary text-font">
-      <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-md">Fangxing Liu</p>
-          <p className="text-small text-default-500">fangxingliu.com</p>
-        </div>
-      </CardHeader>
-      <Divider/>
-      <CardBody>
-        <p>
-          Under construction.
-        </p>
-      </CardBody>
-      <Divider/>
-    </Card>
+    <div className='flex justify-center'>
+      <Card className="bg-primary text-font">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-md">Fangxing Liu</p>
+            <p className="text-small text-default-500">fangxingliu.com</p>
+          </div>
+        </CardHeader>
+        <Divider/>
+        <CardBody>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, harum quos! Et porro aliquid quae error sapiente omnis voluptates debitis blanditiis, quasi alias recusandae, maiores dolor ut dolores molestiae vero?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, harum quos! Et porro aliquid quae error sapiente omnis voluptates debitis blanditiis, quasi alias recusandae, maiores dolor ut dolores molestiae vero?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, harum quos! Et porro aliquid quae error sapiente omnis voluptates debitis blanditiis, quasi alias recusandae, maiores dolor ut dolores molestiae vero?
+          </p>
+        </CardBody>
+        <Divider/>
+      </Card>
+    </div>
   )
 }
