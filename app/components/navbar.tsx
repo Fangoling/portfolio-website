79837b8ac5c8146bab68e19bf8c63777ed4986cd
fxliu <ge83toc@mytum.de>
2024-07-12 01:13:21 +0200

    still can"t hide components depending on screensize
    removed hidden from navbar items

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,69 +9,82 @@ import {
   NavbarMenu,
   NavbarMenuItem,
   NavbarMenuToggle,
-  menu
 } from "@nextui-org/react";
 import React from "react";
 
 export default function NavbarComponent() {
   const pathname = usePathname();
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = {
-    "Home": "/",
-    "About": "/about",
-    "Portfolio": "/portfolio",
-    "Contact": "/contact"
-  } 
+    Home: "/",
+    About: "/about",
+    Portfolio: "/portfolio",
+    Contact: "/contact",
+  };
 
   return (
-      <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-card">
-        <NavbarContent>
-          <NavbarMenuToggle
-            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-            className="block xl:hidden"
-          />
-          <NavbarBrand className="h-full">
-            <p className="text-xl text-font no-underline font-semibold">
-              Fangxing.
-            </p>
-          </NavbarBrand>
-        </NavbarContent>
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-card">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="block hidden:xl"
+        />
+        <NavbarBrand className="h-full">
+          <p className="text-xl text-font no-underline font-semibold">
+            Fangxing.
+          </p>
+        </NavbarBrand>
+      </NavbarContent>
+
         <NavbarContent className="hidden xl:block gap-4 flex-wrap" justify="center">
-          <NavbarItem isActive={pathname === '/'}>
-            <Link aria-current="page" color="foreground" href="/" className="text-xl text-font">
+          <NavbarItem isActive={pathname === "/"}>
+            <Link
+              aria-current="page"
+              color="foreground"
+              href="/"
+              className="text-xl text-font"
+            >
               Home
             </Link>
           </NavbarItem>
-          <NavbarItem isActive={pathname === '/about'}>
-            <Link color="foreground" href="/about" className="text-xl text-font">
+          <NavbarItem isActive={pathname === "/about"}>
+            <Link
+              color="foreground"
+              href="/about"
+              className="text-xl text-font"
+            >
               About
             </Link>
           </NavbarItem>
-          <NavbarItem isActive={pathname === '/portfolio'}>
-            <Link color="foreground" href="/portfolio" className="text-xl text-font">
+          <NavbarItem isActive={pathname === "/portfolio"}>
+            <Link
+              color="foreground"
+              href="/portfolio"
+              className="text-xl text-font"
+            >
               Portfolio
             </Link>
           </NavbarItem>
-          <NavbarItem isActive={pathname === '/contact'}>
-            <Link color="foreground" href="/contact" className="text-xl text-font">
+          <NavbarItem isActive={pathname === "/contact"}>
+            <Link
+              color="foreground"
+              href="/contact"
+              className="text-xl text-font"
+            >
               Contact
             </Link>
           </NavbarItem>
         </NavbarContent>
-        <NavbarMenu className="bg-primary">
-          {Object.entries(menuItems).map(([label, path]) => (
-            <NavbarMenuItem key={label}>
-              <Link
-                className="w-full text-font"
-                href={path}
-                size="lg"
-                >
-                {label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </NavbarMenu>
-      </Navbar>
+      <NavbarMenu className="bg-primary">
+        {Object.entries(menuItems).map(([label, path]) => (
+          <NavbarMenuItem key={label}>
+            <Link className="w-full text-font" href={path} size="lg">
+              {label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
