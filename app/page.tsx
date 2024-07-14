6eed19422555b8ea70939d4423640bc5806f2c66
fxliu <ge83toc@mytum.de>
2024-07-15 00:15:24 +0200

    changed h3 to h2 on front page
    optimized page for accessibility, headings need to be in sequentially descending order

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import './global.css'
-import Image from 'next/image'
-import {Button} from '@nextui-org/button'
 
 export default function Home() {
   return (
     <main className='flex justify-center'> 
       <div className="home-content">
         <h1>Hi, I&apos;m Fangxing Liu</h1>
-        <h3>Computer Science Student</h3>
+        <h2>Computer Science Student</h2>
         <p>As an aspiring programmer with proficiency in Python, Android Jetpack and Typescript. 
         I look forward to gain lots of experience and enhance my skills. 
         <br />Currently, I am pursuing my studies at the Technical University of Munich.</p>
